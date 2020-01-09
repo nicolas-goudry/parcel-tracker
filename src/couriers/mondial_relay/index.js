09a5ff34ac7a213b3710b45b31e4057945409f5a
Nicolas Goudry <goudry.nicolas@gmail.com>
2020-01-09 14:58:02 +0100

    Update zip code rules
    It appears that zip code is required only for tracking numbers of 8 characters

--- src/couriers/mondial_relay/index.js
+++ src/couriers/mondial_relay/index.js
@@ -9,7 +9,7 @@ export const metadata = {
   id: 'MONDIAL_RELAY',
   label: 'Mondial Relay',
   matcher: [
-    /\b(\d{8})\b/i, // 01234567
+    /\b(\d{8})\b/i, // 01234567 ==> requires zip code
     /\b(\d{10})\b/i, // 0123456789
     /\b(\d{12})\b/i // 012345678901
   ]
@@ -17,14 +17,21 @@ export const metadata = {
 
 const fetchParams = (number) => {
   const [trackingNumber, zipCode] = number.split(':')
+  let url
 
-  if (!zipCode) {
-    throw errors.zipCode
+  if (trackingNumber.length === 8) {
+    if (!zipCode) {
+      throw errors.zipCode
+    }
+
+    url = `http://www.mondialrelay.fr/suivi-de-colis?codePostal=${zipCode}&numeroExpedition=${trackingNumber}`
+  } else {
+    url = `http://www.mondialrelay.fr/suivi-de-colis?numeroExpedition=${trackingNumber}`
   }
 
   return {
     method: 'get',
-    url: `http://www.mondialrelay.fr/suivi-de-colis?codePostal=${zipCode}&numeroExpedition=${trackingNumber}`,
+    url,
     responseType: 'text'
   }
 }
