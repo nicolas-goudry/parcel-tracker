58c37976ad78ceddc89f54d27f68e338fdd27d22
Nicolas Goudry <goudry.nicolas@gmail.com>
2020-01-27 12:16:40 +0100

    Update main entry point

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import 'moment/locale/fr'
+
 export { default as identify } from './identifier'
-export { default as track } from './tracker'
-export { metadata as couriers } from './couriers'
+export { default as Chronopost } from './couriers/chronopost'
+export { default as ColisPrive } from './couriers/colis_prive'
+export { default as Colissimo } from './couriers/colissimo'
+export { default as DHL } from './couriers/dhl'
+export { default as DPD } from './couriers/dpd'
