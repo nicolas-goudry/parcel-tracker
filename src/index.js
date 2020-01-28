ad75633470406cfb91a4ea4df91e40c44dea3f7d
Nicolas Goudry <goudry.nicolas@gmail.com>
2020-01-28 12:53:44 +0100

    Update module exportations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import 'moment/locale/fr'
 
-export { default as identify } from './identifier'
-export { default as Chronopost } from './couriers/chronopost'
-export { default as ColisPrive } from './couriers/colis_prive'
-export { default as Colissimo } from './couriers/colissimo'
-export { default as DHL } from './couriers/dhl'
-export { default as DPD } from './couriers/dpd'
+import * as couriers from './couriers'
+import identify from './identifier'
+
+export { identify, couriers }
